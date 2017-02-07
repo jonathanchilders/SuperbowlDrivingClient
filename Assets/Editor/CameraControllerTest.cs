a6e6137ebd206bcae12f91442c6fc00243b5a070
Jonathan Childers <jonathanchilders65@gmail.com>
2017-02-06 20:21:21 -0800

    Barebones driving with standard assets imported

---
+++ Assets/Editor/CameraControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class CameraControllerTest 
+{
+	[Test]
+	public void TestCamera() 
+	{
+		
+	}
+}
---
+++ Assets/Editor/GameOrchestratorTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class GameOrchestratorTest 
+{
+	[Test]
+	public void EditorTest() 
+	{
+		Assert.AreEqual (1, 1);
+	}
+}
